a8c228f29ff29b1255d344fe1cee226c8eb65305
Guy Marshall <guymarshall9@gmail.com>
2023-12-27 22:15:27 +0000

    UPDATE: Show/hide url or text based on switch checked value

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-document.querySelector('form').addEventListener('submit', (event) => {
-    event.preventDefault();
-    const url = document.getElementById('urlInput').value;
-    const text = document.getElementById('exampleTextarea').value;
-    console.log('URL:', url);
-    console.log('Text:', text);
+function initialiseUrlTextChoice() {
+    const urlOrText = document.getElementById('url-or-text');
+
+    showHideUrlText(urlOrText.checked)
+
+    urlOrText.addEventListener('change', (event) => {
+        const switchOn = event.target.checked;
+
+        showHideUrlText(switchOn);
+    });
+}
+
+function showHideUrlText(switchOn) {
+    const leftSection = document.querySelector('.left-section');
+    const rightSection = document.querySelector('.right-section');
+    const urlInput = document.getElementById('url-input');
+    const text = document.getElementById('text');
+
+    if (switchOn) {
+        leftSection.style.display = 'none';
+        urlInput.disabled = true;
+        rightSection.style.display = 'block';
+        text.disabled = false;
+    } else {
+        rightSection.style.display = 'none';
+        text.disabled = true;
+        leftSection.style.display = 'block';
+        urlInput.disabled = false;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    initialiseUrlTextChoice();
 });
